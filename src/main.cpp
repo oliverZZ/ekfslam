c37e5868335e9df2e22ed75a7fb30fae22c97cd8
msallak <55896910+msallak@users.noreply.github.com>
2019-11-14 11:23:21 -0800

    Main file tests Inititalize function2

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 
 int main(int argc, char* argv[]) {
 
-if (argc != 3) {
-  //These are our usage instructions for inputing incorrect # of inputs
-  std::cerr << "Files required are sensor.dat and world.dat";
-  return 1;
+  if (argc != 3) {
+    //These are our usage instructions for inputing incorrect # of inputs
+    std::cerr << "Files required are sensor.dat and world.dat";
+    return 1;
 
+  Mapper mapper;
+  mapper.initialize(argv[2]);
   
 
-return 0;
+  return 0;
 }
