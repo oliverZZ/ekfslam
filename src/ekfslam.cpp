e9c04ea60cc28edc0c33511f3531069405e5a175
OliverZZ <hczheng@ucdavis.edu>
2019-11-22 23:36:41 -0800

    Last Version

--- src/ekfslam.cpp
+++ src/ekfslam.cpp
@@ -5,14 +5,15 @@ EKFSLAM::EKFSLAM() {
 }
 
 EKFSLAM::EKFSLAM(unsigned int landmark_size, unsigned int robot_pose_size, float _motion_noise) {
+      //This function creates mu, robotSigma, mapSigma and other thigns in the big matrx
 
       int N = landmark_size;
       int r = robot_pose_size;
-      mu          = VectorXd::Zero(2*N + r); // Full State Vector
-      robotSigma    = MatrixXd::Zero(r, r); // Covariance Matrix for robot state variables
-      robMapSigma = MatrixXd::Zero(r, 2*N); // Covariance Matrix for robot to landmarks
-      mapSigma    = INF*MatrixXd::Identity(2*N, 2*N); // Covariances of landmark positions wrt to each other
-      Sigma       = MatrixXd::Zero(2*N + r, 2*N + r);// Full Covariance Matrix
+      this->mu          = VectorXd::Zero(2*N + r); // Full State Vector
+      this->robotSigma    = MatrixXd::Zero(r, r); // Covariance Matrix for robot state variables
+      this->robMapSigma = MatrixXd::Zero(r, 2*N); // Covariance Matrix for robot to landmarks
+      this->mapSigma    = INF*MatrixXd::Identity(2*N, 2*N); // Covariances of landmark positions wrt to each other
+      this->Sigma       = MatrixXd::Zero(2*N + r, 2*N + r);// Full Covariance Matrix
 
       Sigma.topLeftCorner(r, r)          = robotSigma;
       Sigma.topRightCorner(r, 2*N)       = robMapSigma;
@@ -25,13 +26,14 @@ EKFSLAM::EKFSLAM(unsigned int landmark_size, unsigned int robot_pose_size, float
       R.topLeftCorner(3,3) << motion_noise, 0, 0,
                               0, motion_noise , 0,
                               0, 0,   motion_noise/10;
-      observedLandmarks.resize(N);
+      observedLandmarks.resize(N); // create the matrix and initialize with all false. No landmark seen at the begining
       fill(observedLandmarks.begin(), observedLandmarks.end(), false);
 }
 
 
 
 void EKFSLAM::Prediction(const OdoReading& motion) {
+  // This function predict the trace of robot according to odoReading
 
       double angle = this->mu(2);
       double r1    = motion.r1; //first rotation
@@ -55,24 +57,29 @@ void EKFSLAM::Prediction(const OdoReading& motion) {
       Sigma.topRightCorner(3, size-3) = Gxt * Sigma.topRightCorner(3, size-3);
       Sigma.bottomLeftCorner(size-3, 3) = Sigma.topRightCorner(3, size-3).transpose();
       Sigma = Sigma + R; // update covariance matrices
+      this->Sigma = Sigma;
+
 }
 
 void EKFSLAM::Correction(const vector<LaserReading>& observation){
+        //This functions update K, sigma and mu according to sensor_info
         int m = observation.size(); // number of measurements
-        int N = observedLandmarks.size();
+        //int N = observedLandmarks.size();
         int sigma_rows = this->Sigma.rows();
+        Eigen::MatrixXd Sigma = this->getSigma();
+
         Eigen::VectorXd pos_mu;
         Eigen::VectorXd robot_mu;
         pos_mu   = mu;
         robot_mu = mu;
 
 
-        Eigen::MatrixXd H              = MatrixXd::Zero(5, 2*N + 3); //observation Jacobian. Size is the same as Fx,j
+        Eigen::MatrixXd H;             // = MatrixXd::Zero(5, 2*N + 3); //observation Jacobian.
         Eigen::MatrixXd Q              = MatrixXd::Identity(2,2)*0.01; // sensor noise matrix
         Eigen::VectorXd Z              = VectorXd::Zero(2);
         Eigen::VectorXd expectedZ      = VectorXd::Zero(2);
 
-        Eigen::MatrixXd LowH           = MatrixXd::Zero(2,5);
+        Eigen::MatrixXd LowH(2,5);
 
 
         for (int i = 0; i < m; i++) {
@@ -82,16 +89,15 @@ void EKFSLAM::Correction(const vector<LaserReading>& observation){
             float     bearing    = reading.bearing;
             Z(0) = range;
             Z(1) = bearing;
-            Eigen::MatrixXd Fxj            = MatrixXd::Zero(5, sigma_rows);
-            Fxj.block<3,3>(0,0) << 1,0,0,
+            Eigen::MatrixXd Fxj = MatrixXd::Zero(5, sigma_rows);
+            Fxj.block<3,3>(0,0) << 1,0,0, // create Fxj matrix
                                    0,1,0,
                                    0,0,1;
             Fxj.block<2,2>(3,2*landmarkId+1) << 1,0,
                                                 0,1;
-            std::cout << "landmarkId"<<landmarkId << '\n';
-            std::cout << Fxj << '\n';
+
             //landmark is not seen before, so to initialize the landmarks
-            if (!observedLandmarks[landmarkId-1]) {
+            if (this->observedLandmarks[landmarkId-1]==false) {
                 pos_mu(0) = robot_mu(0) + range*cos(mu(2) + robot_mu(2));
                 pos_mu(1) = robot_mu(1) + range*sin(mu(2) + robot_mu(2));
 
@@ -105,7 +111,7 @@ void EKFSLAM::Correction(const vector<LaserReading>& observation){
                 pos_mu(0) = robot_mu(landmarkId*2+1);
                 pos_mu(1) = robot_mu(landmarkId*2+2);
               }
-             //Eigen::MatrixXd Delta = MatrixXd::Zero(2,1);
+
              double Deltax = pos_mu(0) - robot_mu(0);// delta x
              double Deltay = pos_mu(1) - robot_mu(1);//delta y
              double q = pow(Deltax, 2) + pow(Deltay, 2);
@@ -115,6 +121,7 @@ void EKFSLAM::Correction(const vector<LaserReading>& observation){
                       Deltay/q,         -1 * Deltax/q,  -q/q, -1*Deltay/q,      Deltax/q;
               H = LowH * Fxj;
 
+
               Eigen::MatrixXd Ht = H.transpose();
               Eigen::MatrixXd HQ = (H*Sigma*Ht) + Q;
               Eigen::MatrixXd Si = HQ.inverse();
@@ -122,14 +129,14 @@ void EKFSLAM::Correction(const vector<LaserReading>& observation){
               Eigen::VectorXd diff = Z - expectedZ;
 
 
-                for (int j = 1; j < diff.size(); j += 2) {
-                        while(diff(j)>M_PI) {
-                          diff(j) = diff(j) - 2*M_PI;
+                //for (int j = 1; j < diff.size(); j += 2) {
+                        while(diff(1)>M_PI) {
+                          diff(1) = diff(1) - 2*M_PI;
                         }
-                        while(diff(j)<M_PI) {
-                          diff(j) = diff(j) + 2*M_PI;
+                        while(diff(1)<M_PI) {
+                          diff(1) = diff(1) + 2*M_PI;
                         }
-                  }
+                  //}
               robot_mu = robot_mu + K * diff;
               while(robot_mu(2)>M_PI) {
                   robot_mu(2) = robot_mu(2) - 2*M_PI;
@@ -142,5 +149,6 @@ void EKFSLAM::Correction(const vector<LaserReading>& observation){
 
         }
 
-        mu = robot_mu;
+        this->mu = robot_mu;
+        this->Sigma = Sigma;
       }
